7f7c6efcabc4fd3ad30b3f63a8f963a085596c1a
PriyanshNub <140973671+PriyanshNub@users.noreply.github.com>
2023-07-30 23:28:52 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="name">
+      <b>
+      <h1>Priyansh Sharaf</h1>
+      </b>
+    </div>
+  );
+}
+
+export default App;
